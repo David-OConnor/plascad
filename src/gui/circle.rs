9f3c83bd7522f4d56d7356478179114509397568
David O'Connor <david@opsplans.org>
2024-07-31 22:17:04 -0400

    Circle now aligns properly

--- src/gui/circle.rs
+++ src/gui/circle.rs
@@ -1,7 +1,7 @@
 //! A module for the circular view of a plasmid
 
 use core::f32::consts::TAU;
-
+use std::cmp::min;
 use eframe::egui::{Align2, Color32, FontFamily, FontId, Frame, Pos2, pos2, Rect, Sense, Shape, Stroke, Ui, vec2};
 use eframe::emath::RectTransform;
 use eframe::epaint::{CircleShape, PathShape};
@@ -12,8 +12,15 @@ use crate::util::{pixel_to_seq_i, seq_i_to_pixel};
 const BACKBONE_COLOR: Color32 = Color32::from_rgb(180, 180, 180);
 const BACKBONE_WIDTH: f32 = 10.;
 
-const TICK_COLOR: Color32 = Color32::from_rgb(220, 220, 220);
+const TICK_COLOR: Color32 = Color32::from_rgb(180, 220, 220);
 const TICK_WIDTH: f32 = 4.;
+
+const TICK_LEN: f32 = 50.; // in pixels.
+const TICK_LEN_DIV_2: f32 = TICK_LEN / 2.;
+const TICK_LABEL_OFFSET: f32 = 10.;
+
+// Of the available width or height (whichever is lower).
+const CIRCLE_SIZE_RATIO: f32 = 0.4;
 
 /// Rreturn the angle in radians of a given sequence index.
 fn seq_i_to_angle(seq_i: usize, seq_len: usize) -> f32 {
@@ -29,6 +36,46 @@ fn angle_to_pixel(angle: f32, radius: f32) -> Pos2 {
     let angle = (angle - TAU/4.);
 
     pos2(angle.cos() * radius, angle.sin() * radius)
+}
+
+/// Draw a tick every 1kbp.
+fn draw_ticks(seq_len: usize, center: Pos2, radius: f32, to_screen: &RectTransform,  ui: &mut Ui) -> Vec<Shape> {
+    let mut result = Vec::new();
+
+    for i_div_1k in 0..seq_len / 1_000 { // todo: Confirm this always rounds down.
+        let i = i_div_1k * 1_000;
+
+        let angle = seq_i_to_angle(i, seq_len);
+        // let intersection = angle_to_pixel(angle, radius) + center.to_vec2();
+
+        let point_inner = angle_to_pixel(angle, radius - TICK_LEN_DIV_2) + center.to_vec2();
+        let point_outer = angle_to_pixel(angle, radius + TICK_LEN_DIV_2) + center.to_vec2();
+
+        result.push(Shape::line_segment(
+            [to_screen * point_inner, to_screen * point_outer],
+            Stroke::new(TICK_WIDTH, TICK_COLOR),
+        ));
+
+        let (label_pt, label_align) = if angle > TAU / 2. {
+            ( point_outer + vec2(-TICK_LABEL_OFFSET, 0.), Align2::RIGHT_CENTER)
+        } else {
+           ( point_outer + vec2(TICK_LABEL_OFFSET, 0.), Align2::LEFT_CENTER)
+        };
+
+        let label = ui.ctx().fonts(|fonts| {
+            Shape::text(
+                fonts,
+                to_screen * label_pt,
+                label_align,
+                i.to_string(),
+                FontId::new(16., FontFamily::Proportional),
+                TICK_COLOR
+            )
+        });
+
+        result.push(label);
+    }
+    result
 }
 
 
@@ -50,7 +97,6 @@ pub fn circle_page(seq: &[Nucleotide], features: &[Feature], ui: &mut Ui) {
 
             };
 
-
             let to_screen = RectTransform::from_to(
                 Rect::from_min_size(Pos2::ZERO, response.rect.size()),
                 response.rect,
@@ -58,45 +104,20 @@ pub fn circle_page(seq: &[Nucleotide], features: &[Feature], ui: &mut Ui) {
 
             let from_screen = to_screen.inverse();
 
-            // todo: Sort out avail height.
-            let center = pos2(ui.available_width() / 2., ui.available_width() / 2.);
+            let rect_size = response.rect.size();
 
-            // todo: Set radius to use the minimum of avial width/avail height, once avail_height works.
-            let radius = ui.available_width() * 0.4;
+            let center = pos2(rect_size.x / 2., rect_size.y / 2.);
 
-            println!("CENTER: {:?}, av h: {}", center, ui.available_height());
+            let width_min = rect_size.x < rect_size.y;
+
+            let radius = if width_min { rect_size.x} else { rect_size.y} * CIRCLE_SIZE_RATIO;
 
             // Draw the backbone circle
-            shapes.push(Shape::Circle(CircleShape::stroke(center, radius, Stroke::new(BACKBONE_WIDTH, BACKBONE_COLOR))));
+            shapes.push(Shape::Circle(CircleShape::stroke(to_screen * center, radius, Stroke::new(BACKBONE_WIDTH, BACKBONE_COLOR))));
 
             let seq_len = seq.len();
 
-            // Draw ticks every 1kbp.
-            for i_div_1k in 0..seq_len / 1_000 { // todo: Confirm this always rounds down.
-                let i = i_div_1k * 1_000;
-
-                let angle = seq_i_to_angle(i, seq_len);
-                let intersection = angle_to_pixel(angle, radius) + center.to_vec2();
-
-                shapes.push(Shape::line_segment(
-                    [intersection, center],
-                    Stroke::new(TICK_WIDTH, TICK_COLOR),
-                ));
-
-                let label = ui.ctx().fonts(|fonts| {
-                    Shape::text(
-                        fonts,
-                        intersection,
-                        Align2::LEFT_CENTER,
-                        i.to_string(),
-                        FontId::new(16., FontFamily::Proportional),
-                        TICK_COLOR
-                    )
-                });
-
-                shapes.push(label);
-
-            }
+            shapes.append(&mut draw_ticks(seq_len, center, radius, &to_screen, ui));
 
             ui.painter().extend(shapes);
         });
